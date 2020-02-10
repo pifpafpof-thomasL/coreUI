d29c5b19c8636f7f61f83b01e0bce843ec9fda93
NG8932F <thomas.lechevalier.external@airbus.com>
2020-02-10 21:31:29 +0100

    with weather

--- src/views/Todo/DayWeather.js
+++ src/views/Todo/DayWeather.js
@@ -1,36 +1,59 @@
-import React, { Component } from "react";
+import React from "react";
 
-class WeekContainer extends React.Component {
+class DayWeather extends React.Component {
   state = {
     fullData: [],
     dailyData: []
+  };
+
+  componentDidMount() {
+    this.fetchWeather();
   }
 
-  componentDidMount = () => {
+  componentDidUpdate(prevProps) {
+    // console.log("this.props, prevProps", prevProps, this.props);
+    if (prevProps.city !== this.props.city) {
+      this.fetchWeather();
+    }
+  }
+
+  fetchWeather() {
+    const { city } = this.props;
     const weatherURL =
       // `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=metric&APPID=6ac65692ee43e6cd36a695242cc7c988`
       //   `http://api.openweathermap.org/data/2.5/forecast?&q=Munich&units=metric&APPID=6ac65692ee43e6cd36a695242cc7c988`
-    // const weatherURL =
-     `http://api.openweathermap.org/data/2.5/forecast?appid=6ac65692ee43e6cd36a695242cc7c988&lang=en&units=metric&q=Munich`
+      // const weatherURL =
+      `http://api.openweathermap.org/data/2.5/forecast?` +
+      `appid=6ac65692ee43e6cd36a695242cc7c988&lang=en&units=metric&q=${city}`;
 
     fetch(weatherURL)
       .then(res => res.json())
       .then(data => {
-        const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
-        this.setState({
-          fullData: data.list,
-          dailyData: dailyData
-        }, () => console.log(this.state))
-      })
+        const dailyData = data.list.filter(reading =>
+          reading.dt_txt.includes("18:00:00")
+        );
+        this.setState(
+          {
+            fullData: data.list,
+            dailyData: dailyData
+          },
+          // () => console.log(this.state)
+        );
+      });
   }
 
   render() {
     return (
       <div>
-        <p>Current temperature is {this.state.dailyData.length ? this.state.fullData[0].main.temp: null}</p>
+        <p>
+          Current temperature is{" "}
+          {this.state.dailyData.length
+            ? this.state.fullData[0].main.temp
+            : null}
+        </p>
       </div>
-    )
+    );
   }
 }
 
-export default WeekContainer;
+export default DayWeather;
